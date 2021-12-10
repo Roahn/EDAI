581a40dcb79e12fc8b0145c3d10bb12f78111da3
Roahn <rohan.magar19@vit.edu>
2021-12-10 17:31:02 +0530

    Readded mysql connection

--- index.js
+++ index.js
@@ -18,6 +18,27 @@ app.get('/products', (req, res) => {
 res.sendFile(path.join(__dirname, '/public/products.html'));
 });
 
+
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+
+
+
+
+
+
 app.listen(3000, () => {
 console.log('Server is up on port 3000');
 });
